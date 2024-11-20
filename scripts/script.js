e85340b0f3afe2af7f8987cdeb956b4e1e83896c
azmaeenmahtab <mahtabazmaeen7@gmail.com>
2024-11-20 08:40:22 +0600

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
 function scrollDownToTicketBookingPage() {
     document.getElementById('ticket-booking-page').scrollIntoView({ behavior: 'smooth' })
 }
+function seatSelection(event) {
+    // changing color
+    console.log(event.target.id)
+    document.getElementById(event.target.id).classList.add('bg-[#1DD100]')
+
+}
+document.addEventListener('mousedown', seatSelection)
