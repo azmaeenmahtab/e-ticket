e9e9c408e8257427466db523063225ca8c6522bd
azmaeenmahtab <mahtabazmaeen7@gmail.com>
2024-11-21 10:59:05 +0600

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,33 @@ function scrollDownToTicketBookingPage() {
     document.getElementById('ticket-booking-page').scrollIntoView({ behavior: 'smooth' })
 }
 function seatSelection(event) {
-    // changing color
-    console.log(event.target.id)
-    document.getElementById(event.target.id).classList.add('bg-[#1DD100]')
-
-    // decrease seat num
-    let availableSeats = valueOfTheElement('available-seats')
-    let newAvailableSeats = availableSeats - 1;
-    setElementValue('available-seats', newAvailableSeats)
-
-    // checking if one is not selecting more than 4 seats
-    let selectedSeat = valueOfTheElement('selectedSeatNum');
-    let newSelectedSeat = selectedSeat + 1;
-    console.log(newSelectedSeat)
-    setElementValue('selectedSeatNum', newSelectedSeat)
-
+    let seat = event.target;
+    if (seat.classList.contains('selected')) {
+        console.log('select');
+        seat.classList.remove('selected');
+        seat.classList.remove('bg-[#1DD100]');
+    } else {
+        console.log('notselect');
+        seat.classList.add('selected');
+        seat.classList.add('bg-[#1DD100]');
+    }
 }
+
 document.addEventListener('mousedown', seatSelection)
+// // selecting seat
+//     console.log(event.target.id)
+//     document.getElementById(event.target.id).classList.add('bg-[#1DD100]')
+
+//     // decrease seat num
+//     let availableSeats = valueOfTheElement('available-seats')
+//     let newAvailableSeats = availableSeats - 1;
+//     setElementValue('available-seats', newAvailableSeats)
+
+//     // checking if one is not selecting more than 4 seats
+//     let selectedSeat = valueOfTheElement('selectedSeatNum');
+//     let newSelectedSeat = selectedSeat + 1;
+//     console.log(newSelectedSeat)
+//     setElementValue('selectedSeatNum', newSelectedSeat)
+//     if (valueOfTheElement('selectedSeatNum') === 4) {
+//         document.addEventListener('mousedown', noSeatWillSelected)
+//     }
--- scripts/utility.js
+++ scripts/utility.js
@@ -13,3 +13,24 @@ function setElementValue(elementId, providedValue) {
     return document.getElementById(elementId).innerText = providedValue;
 
 }
+// add class to element
+function addClassToElement(elementId, classname) {
+    document.getElementById(elementId).classList.add(classname)
+}
+// remove  class to element
+function removeClassFromElement(elementId, classname) {
+    document.getElementById(elementId).classList.remove(classname)
+}
+// selecting seat
+function selectingSeat() {
+    document.getElementById(event.target.id).classList.add('bg-[#1DD100]')
+    document.getElementById(event.target.id).classList.add('selected')
+}
+// no seat selection function
+function noSeatWillSelected() {
+    // undo color 
+    document.getElementById(event.target.id).classList.remove('bg-[#1DD100]')
+    document.getElementById(event.target.id).classList.remove('notselected')
+    // selected seat number fix
+    setElementValue('selectedSeatNum', 4)
+}
